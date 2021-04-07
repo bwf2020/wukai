b57ae8995f2b37208f28b8dd8761727f5d449eb7
niuxiaoxuan <niuxiaoxuan@home>
2021-04-07 16:16:14 +0800

    123

---
+++ src/cn/Hello1.java
@@ -0,0 +1,13 @@
+package cn;
+
+import java.util.Scanner;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(");
+
+	}
+
+}
---
+++ src/test1.java
@@ -0,0 +1,12 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		System.out.print('a'+0);
+		System.out.print('A'+0);
+		System.out.print('\65');
+		System.out.print('\u0035');
+		
+	}
+
+}
